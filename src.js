7b13db53d2df3abe6e8a6aea2c1305f30b3856b0
NickRojasP <91437679+NickRojasP@users.noreply.github.com>
2021-12-01 11:05:26 -0500

    subiendo archivos base sprint 4 nick

--- src.js
+++ src.js
@@ -1,43 +1,20 @@
-const registrarMunicipio = async ( 
-    municipio,
-    departamento,
-    numeroCasos,
-    numeroMujeres,
-    numeroHombres,
-    numeroRecuperados
-)   => {
+const express=require("express");
+const app=express();
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-    let response = await fetch(
-        "https://misiontic2022upb.vercel.app/api/epidemiologic-monitor/villages",
-        {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                "municipio": municipio, "departamento": departamento, "numeroCasos":numeroCasos, "numeroMujeres":numeroMujeres, "numeroHombres": numeroHombres, "numeroRecuperados": numeroRecuperados
-            }),
-        }
-    );
-    let result = await response.json();
-    return result
-};
-const filtrarEInsertarNoticias = async () => {
+app.get("tu ruta aqui",(req,res)=>{
+    //get news
+});
 
-    let response = await fetch(
-        "https://misiontic2022upb.vercel.app/api/epidemiologic-monitor/news"
-    );
-    let noticiasAPI = await response.json();
-    const noticiasFiltradas = [];
-    for (let i = 0, h = 0, l = noticiasAPI.length; i< l; i++){
-        if(noticiasAPI[i].titulo.indexOf("coronavirus")!==-1 ||noticiasAPI[i].titulo.indexOf("covid19")!==-1 ||noticiasAPI[i].contenido.indexOf("coronavirus")!==-1 ||noticiasAPI[i].contenido.indexOf("covid19")!==-1){
-        noticiasFiltradas[h]=noticiasAPI[i];
-        h +=1;
-        }
+app.get(
+    "tu ruta aqui",
+    (req,res)=>{
+        //get news
     }
-    return noticiasFiltradas
-};
+);
 
-module.exports.filtrarEInsertarNoticias = filtrarEInsertarNoticias;
-module.exports.registrarMunicipio = registrarMunicipio;
+app.post("tu ruta aqui",(req,res)=>{
+    //create village
+});
+module.exports=app;
